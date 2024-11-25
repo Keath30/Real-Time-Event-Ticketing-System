606900f99baadff00a100ef8b6301bfbb9eaa060
KM <keathmel1130@gmail.com>
2024-11-25 12:32:39 +0530

    Added configuration classes

---
+++ BackEnd/ticket-booking-system/src/main/java/com/iit/ticket_booking_system/cli/Main.java
@@ -0,0 +1,14 @@
+package com.iit.ticket_booking_system.cli;
+
+import com.iit.ticket_booking_system.cli.controller.InputController;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        InputController.start();
+
+    }
+
+
+}
---
+++ BackEnd/ticket-booking-system/src/main/java/com/iit/ticket_booking_system/cli/controller/ConfigurationManager.java
@@ -0,0 +1,50 @@
+package com.iit.ticket_booking_system.cli.controller;
+
+import com.google.gson.Gson;
+import com.iit.ticket_booking_system.cli.config.Configuration;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class ConfigurationManager {
+
+    private static final Gson gson = new Gson();
+
+    public static void saveConfiguration(Configuration configuration, String file){
+        try(FileWriter writer = new FileWriter(file)){
+            gson.toJson(configuration, writer);
+            System.out.println("Configuration saved to: " + file);
+
+        }catch (IOException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void saveConfigurationToTextFile(Configuration config, String textFile) {
+        String content = "Total Tickets: " + config.getTotalTickets() + "\n" +
+                "Ticket Release Rate: " + config.getTicketReleaseRate() + "\n" +
+                "Customer Retrieval Rate: " + config.getCustomerRetrievalRate() + "\n" +
+                "Maximum Ticket Capacity: " + config.getMaxTicketCapacity() + "\n";
+
+        try {
+            Files.write(Paths.get(textFile), content.getBytes());
+            System.out.println("Configuration saved to " + textFile);
+        } catch (IOException e) {
+            System.err.println("Error saving configuration to text file: " + e.getMessage());
+        }
+    }
+
+    public static Configuration loadConfiguration(String file){
+        try (FileReader reader = new FileReader(file)) {
+            return gson.fromJson(reader, Configuration.class);
+
+        }catch (IOException e){
+            System.out.println("Error: " + e.getMessage());
+            return null;
+        }
+    }
+
+}
